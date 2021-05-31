fec98ee7a836bd6f12a7815eae58fc09dcfa209b
Raymond Ng <rayng86@gmail.com>
2021-05-31 11:33:40 -0400

    prevent anything but numbers on key down

--- src/components/TimerInput.tsx
+++ src/components/TimerInput.tsx
@@ -13,6 +13,12 @@ const TimerInput = ({
   handleInput,
   isDisabled,
 }: TimerInputProps) => {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    const ALLOWED_CHARS_REGEXP = /[0-9/]+/;
+    if (!ALLOWED_CHARS_REGEXP.test(e.key)) {
+      e.preventDefault();
+    }
+  };
   return (
     <input
       className="input input-bordered text-black"
@@ -21,6 +27,7 @@ const TimerInput = ({
       placeholder={placeholder}
       value={minutesInput}
       onChange={handleInput}
+      onKeyDown={handleKeyDown}
       disabled={isDisabled}
     />
   );
